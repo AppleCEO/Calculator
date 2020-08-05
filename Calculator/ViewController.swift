65b30e3ee7898d5cfe76133cca8d8b25463002c7
joon-ho kil <kiljh@me.com>
2020-08-05 23:47:07 +0900

    디스플레이 추가

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
   
+  @IBOutlet weak var display: UILabel!
+  
   @IBAction func touchDigit(_ sender: UIButton) {
     let digit = sender.currentTitle!
     print("touched \(digit) digit")
